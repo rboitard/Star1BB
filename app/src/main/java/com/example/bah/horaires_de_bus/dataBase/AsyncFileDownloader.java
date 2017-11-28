ad14e12c848d48fcc31a97fb3ccc386d41666001
romain <rboitard@gmail.com>
2017-11-28 17:07:44 +0100

    Update AsyncFileDowloader
    now, files are be unzip after downloading

--- app/src/main/java/com/example/bah/horaires_de_bus/dataBase/AsyncFileDownloader.java
+++ app/src/main/java/com/example/bah/horaires_de_bus/dataBase/AsyncFileDownloader.java
@@ -7,17 +7,24 @@ import android.util.Log;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by romain on 28/11/2017.
  */
 
 public class AsyncFileDownloader extends AsyncTask<String, Void, String> {
+
+    private File myZip;
     @Override
     protected String doInBackground(String... params) {
         File dir = new File(Environment.getExternalStorageDirectory() + "/");
@@ -53,6 +60,8 @@ public class AsyncFileDownloader extends AsyncTask<String, Void, String> {
                 buffer.write(data,0,current);
             }
 
+            this.myZip = file;
+
             FileOutputStream fos = new FileOutputStream(file);
             fos.write(buffer.toByteArray());
             fos.close();
@@ -70,6 +79,53 @@ public class AsyncFileDownloader extends AsyncTask<String, Void, String> {
 
     @Override
     protected void onPostExecute(String message) {
+        try {
+            unzip("horraire.zip");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("done");
+    }
+
+    public void unzip(String zipFile) throws IOException {
+        try {
+            File dir = new File(Environment.getExternalStorageDirectory() + "/");
+            if (dir.exists() == false) {
+                dir.mkdirs();
+            }
+            ZipInputStream zin = new ZipInputStream(new FileInputStream(this.myZip));
+            try {
+                ZipEntry ze = null;
+                while ((ze = zin.getNextEntry()) != null) {
+                    String path =dir.getPath() + "/" + ze.getName();
+                    System.out.println("path : " + dir.getPath());
+
+                    if (ze.isDirectory()) {
+                        File unzipFile = this.myZip;
+                        if(!unzipFile.isDirectory()) {
+                            unzipFile.mkdirs();
+                        }
+                    }
+                    else {
+                        FileOutputStream fout = new FileOutputStream(path, false);
+                        try {
+                            for (int c = zin.read(); c != -1; c = zin.read()) {
+                                fout.write(c);
+                            }
+                            zin.closeEntry();
+                        }
+                        finally {
+                            fout.close();
+                        }
+                    }
+                }
+            }
+            finally {
+                zin.close();
+            }
+        }
+        catch (Exception e) {
+            Log.e(TAG, "Unzip exception", e);
+        }
     }
 }
