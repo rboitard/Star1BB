5d58cdd5f62770a7edc43a71ef4ba2e64351c233
Bah <abdourahmanebah273@yahoo.fr>
2018-01-04 10:35:27 +0100

    Update StarProvider

--- app/src/main/java/fr/istic/starproviderBB/StarProvider.java
+++ app/src/main/java/fr/istic/starproviderBB/StarProvider.java
@@ -18,7 +18,7 @@ import fr.istic.database.db.Database;
  * Created by Bah on 30/12/2017.
  */
 
-public class StarProvider extends ContentProvider implements StarContract {
+public class StarProvider extends ContentProvider {
 
 
     private static  final  int QUERY_ALL_DATA_BUS_ROUTE_TABLE = 1;
@@ -43,28 +43,28 @@ public class StarProvider extends ContentProvider implements StarContract {
 
          /************* Bus route **************************/
 
-        URI_MATCHER.addURI(StarContract.AUTHORITY, BusRoutes.CONTENT_PATH,QUERY_ALL_DATA_BUS_ROUTE_TABLE);
-        URI_MATCHER.addURI(StarContract.AUTHORITY, BusRoutes.CONTENT_PATH +"/#",QUERY_BY_DATA_ID_BUS_ROUTE_TABLE);
+        URI_MATCHER.addURI(StarContract.AUTHORITY, StarContract.BusRoutes.CONTENT_PATH,QUERY_ALL_DATA_BUS_ROUTE_TABLE);
+        URI_MATCHER.addURI(StarContract.AUTHORITY, StarContract.BusRoutes.CONTENT_PATH +"/#",QUERY_BY_DATA_ID_BUS_ROUTE_TABLE);
 
         /*************** Calendar *****************************/
 
-        URI_MATCHER.addURI(StarContract.AUTHORITY, Calendar.CONTENT_PATH,QUERY_ALL_DATA_CALENDAR_TABLE);
-        URI_MATCHER.addURI(StarContract.AUTHORITY, Calendar.CONTENT_PATH +"/#",QUERY_BY_DATA_ID_CALENDAR_TABLE);
+        URI_MATCHER.addURI(StarContract.AUTHORITY, StarContract.Calendar.CONTENT_PATH,QUERY_ALL_DATA_CALENDAR_TABLE);
+        URI_MATCHER.addURI(StarContract.AUTHORITY, StarContract.Calendar.CONTENT_PATH +"/#",QUERY_BY_DATA_ID_CALENDAR_TABLE);
 
         /*** ********** Stop ************************************/
 
-        URI_MATCHER.addURI(StarContract.AUTHORITY, Stops.CONTENT_PATH,QUERY_ALL_DATA_STOPS_TABLE);
-        URI_MATCHER.addURI(StarContract.AUTHORITY, Stops.CONTENT_PATH +"/#",QUERY_BY_DATA_ID_STOPS_TABLE);
+        URI_MATCHER.addURI(StarContract.AUTHORITY, StarContract.Stops.CONTENT_PATH,QUERY_ALL_DATA_STOPS_TABLE);
+        URI_MATCHER.addURI(StarContract.AUTHORITY, StarContract.Stops.CONTENT_PATH +"/#",QUERY_BY_DATA_ID_STOPS_TABLE);
 
         /************* Stop Time *********************************/
 
-        URI_MATCHER.addURI(StarContract.AUTHORITY, StopTimes.CONTENT_PATH,QUERY_ALL_DATA_STOP_TIME_TABLE);
-        URI_MATCHER.addURI(StarContract.AUTHORITY, StopTimes.CONTENT_PATH +"/#",QUERY_BY_DATA_ID_STOP_TIME_TABLE);
+        URI_MATCHER.addURI(StarContract.AUTHORITY, StarContract.StopTimes.CONTENT_PATH,QUERY_ALL_DATA_STOP_TIME_TABLE);
+        URI_MATCHER.addURI(StarContract.AUTHORITY, StarContract.StopTimes.CONTENT_PATH +"/#",QUERY_BY_DATA_ID_STOP_TIME_TABLE);
 
         /*** ***********Trips**********************************/
 
-        URI_MATCHER.addURI(StarContract.AUTHORITY, Trips.CONTENT_PATH,QUERY_ALL_DATA_TRIPS_TABLE);
-        URI_MATCHER.addURI(StarContract.AUTHORITY, Trips.CONTENT_PATH +"/#",QUERY_BY_DATA_ID_TRIPS_TABLE);
+        URI_MATCHER.addURI(StarContract.AUTHORITY, StarContract.Trips.CONTENT_PATH,QUERY_ALL_DATA_TRIPS_TABLE);
+        URI_MATCHER.addURI(StarContract.AUTHORITY, StarContract.Trips.CONTENT_PATH +"/#",QUERY_BY_DATA_ID_TRIPS_TABLE);
     }
 
     @Override
@@ -77,38 +77,39 @@ public class StarProvider extends ContentProvider implements StarContract {
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
         SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+        database = new Database(getContext());
         SQLiteDatabase db = database.getReadableDatabase();
         switch (URI_MATCHER.match(uri))
         {
             case QUERY_ALL_DATA_BUS_ROUTE_TABLE :
-                builder.setTables(BusRoutes.CONTENT_PATH);
+                builder.setTables(StarContract.BusRoutes.CONTENT_PATH);
                 break;
             case QUERY_BY_DATA_ID_BUS_ROUTE_TABLE:
-                builder.setTables(BusRoutes.CONTENT_PATH);
+                builder.setTables(StarContract.BusRoutes.CONTENT_PATH);
                 break;
             case QUERY_ALL_DATA_CALENDAR_TABLE :
-                builder.setTables(Calendar.CONTENT_PATH);
+                builder.setTables(StarContract.Calendar.CONTENT_PATH);
                 break;
             case QUERY_BY_DATA_ID_CALENDAR_TABLE:
-                builder.setTables(Calendar.CONTENT_PATH);
+                builder.setTables(StarContract.Calendar.CONTENT_PATH);
                 break;
             case QUERY_ALL_DATA_STOPS_TABLE :
-                builder.setTables(Stops.CONTENT_PATH);
+                builder.setTables(StarContract.Stops.CONTENT_PATH);
                 break;
             case QUERY_BY_DATA_ID_STOPS_TABLE:
-                builder.setTables(Stops.CONTENT_PATH);
+                builder.setTables(StarContract.Stops.CONTENT_PATH);
                 break;
             case QUERY_ALL_DATA_STOP_TIME_TABLE :
-                builder.setTables(StopTimes.CONTENT_PATH);
+                builder.setTables(StarContract.StopTimes.CONTENT_PATH);
                 break;
             case QUERY_BY_DATA_ID_STOP_TIME_TABLE:
-                builder.setTables(StopTimes.CONTENT_PATH);
+                builder.setTables(StarContract.StopTimes.CONTENT_PATH);
                 break;
             case QUERY_ALL_DATA_TRIPS_TABLE :
-                builder.setTables(Trips.CONTENT_PATH);
+                builder.setTables(StarContract.Trips.CONTENT_PATH);
                 break;
             case QUERY_BY_DATA_ID_TRIPS_TABLE:
-                builder.setTables(Trips.CONTENT_PATH);
+                builder.setTables(StarContract.Trips.CONTENT_PATH);
                 break;
             default:
                 throw new IllegalArgumentException(
@@ -130,29 +131,29 @@ public class StarProvider extends ContentProvider implements StarContract {
         switch (URI_MATCHER.match(uri))
         {
             case QUERY_ALL_DATA_BUS_ROUTE_TABLE :
-                return BusRoutes.CONTENT_TYPE;
+                return StarContract.BusRoutes.CONTENT_TYPE;
             case QUERY_BY_DATA_ID_BUS_ROUTE_TABLE:
-               return BusRoutes.CONTENT_ITEM_TYPE;
+               return StarContract.BusRoutes.CONTENT_ITEM_TYPE;
 
             case QUERY_ALL_DATA_CALENDAR_TABLE :
-                return Calendar.CONTENT_TYPE;
+                return StarContract.Calendar.CONTENT_TYPE;
             case QUERY_BY_DATA_ID_CALENDAR_TABLE:
-                return Calendar.CONTENT_ITEM_TYPE;
+                return StarContract.Calendar.CONTENT_ITEM_TYPE;
 
             case QUERY_ALL_DATA_STOPS_TABLE :
-                return Stops.CONTENT_TYPE;
+                return StarContract.Stops.CONTENT_TYPE;
             case QUERY_BY_DATA_ID_STOPS_TABLE:
-                return Stops.CONTENT_ITEM_TYPE;
+                return StarContract.Stops.CONTENT_ITEM_TYPE;
 
             case QUERY_ALL_DATA_STOP_TIME_TABLE :
-                return StopTimes.CONTENT_TYPE;
+                return StarContract.StopTimes.CONTENT_TYPE;
             case QUERY_BY_DATA_ID_STOP_TIME_TABLE:
-                return StopTimes.CONTENT_ITEM_TYPE;
+                return StarContract.StopTimes.CONTENT_ITEM_TYPE;
 
             case QUERY_ALL_DATA_TRIPS_TABLE :
-                return Trips.CONTENT_TYPE;
+                return StarContract.Trips.CONTENT_TYPE;
             case QUERY_BY_DATA_ID_TRIPS_TABLE:
-                return Trips.CONTENT_ITEM_TYPE;
+                return StarContract.Trips.CONTENT_ITEM_TYPE;
 
             default:
                 return null;
